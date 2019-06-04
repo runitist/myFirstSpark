2b2617a292df3380c8d7a62287c876b644280418
wiser <runitist@gmail.com>
2019-06-04 12:57:52 +0900

    템플릿 엔진을 이용한 동적 html 변형

--- src/main/java/com/example/demo/HtmlSpark.java
+++ src/main/java/com/example/demo/HtmlSpark.java
@@ -1,17 +1,34 @@
 package com.example.demo;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.staticFiles;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class HtmlSpark {
 	public static void main(String[] args) {
 		staticFiles.location("/static");
-		
+
 		get("/hello", (req, res) -> {
-			return "Hello : "+req.queryParams("name")+", age : "+ req.queryParams("age");
+			return "Hello : " + req.queryParams("name") + ", age : " + req.queryParams("age");
 		});
-		
+
 		post("/hello", (req, res) -> {
-			return "post Hello : "+req.queryParams("name")+", age : "+ req.queryParams("age");
+			Map<String, Object> model = new HashMap<String, Object>();
+			model.put("name", req.queryParams("name"));
+			model.put("age", req.queryParams("age"));
+
+			return render(model, "result.html");
 		});
+	}
+
+	public static String render(Map<String, Object> model, String templatePath) {
+
+		return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
 	}
 }
