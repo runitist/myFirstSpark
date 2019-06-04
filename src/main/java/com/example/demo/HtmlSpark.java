14c0bd3947d477bbf82b26ff20a300a6d3f966a7
wiser <runitist@gmail.com>
2019-06-04 12:00:27 +0900

    spark html 전달 방식

--- src/main/java/com/example/demo/HtmlSpark.java
+++ src/main/java/com/example/demo/HtmlSpark.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import static spark.Spark.*;
 
-public class HelloWorld {
+public class HtmlSpark {
 	public static void main(String[] args) {
+		staticFiles.location("/static");
+		
 		get("/hello", (req, res) -> {
 			return "Hello : "+req.queryParams("name")+", age : "+ req.queryParams("age");
+		});
+		
+		post("/hello", (req, res) -> {
+			return "post Hello : "+req.queryParams("name")+", age : "+ req.queryParams("age");
 		});
 	}
 }
